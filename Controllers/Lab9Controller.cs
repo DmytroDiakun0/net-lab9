b67c1908e516ebbde01a63209bcac4e166ec3cdc
DmytroDiakun0 <diakun.dm@gmail.com>
2024-03-02 23:14:22 +0200

    net lab9

---
+++ Controllers/Lab9Controller.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class Lab9Controller : Controller
+    {
+        public IActionResult FirstViewMethod()
+        {
+            List<string> vegetables = GetVegetablesList();
+            return View(vegetables);
+        }
+
+        public ActionResult SecondViewMethod()
+        {
+            return View(GetVegetablesList().OrderBy(x => x).ToList());
+        }
+
+        public ActionResult ThirdViewMethod()
+        {
+            return View(GetVegetablesList().OrderBy(x => x).ToList());
+        }
+
+        public List<string> GetVegetablesList()
+        {
+            List<string> vegetables = new List<string>();
+            vegetables.Add("Томат");
+            vegetables.Add("Огірок");
+            vegetables.Add("Картопля");
+            vegetables.Add("Кабачок");
+            vegetables.Add("Баклажан");
+            vegetables.Add("Капуста");
+            vegetables.Add("Броколі");
+
+            return vegetables;
+        }
+    }
+}
